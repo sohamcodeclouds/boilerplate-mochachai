09c03a5d239a6ead7d6deeeeef497a24051b75c6
CODECLOUDS-SOHAM <codeclouds-soham@CISPL-IO-745.local>
2023-12-14 17:23:33 +0530

    10 assertion passed

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -3,30 +3,36 @@ const assert = chai.assert
 
 suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
-    /** 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. **/
+    // #1
     test('#isNull, #isNotNull', function () {
       assert.isNull(
         null,
-        'this is an optional error description - e.g. null is null'
+        'This is an optional error description - e.g. null is null'
       )
       assert.isNotNull(1, '1 is not null')
     })
-
-    /*
-    // #3
-    test('#isOk, #isNotOk', function () {
-      assert.fail(null, 'null is falsey')
-      assert.fail("I'm truthy", 'A string is truthy')
-      assert.fail(true, 'true is truthy')
+    // #2
+    test('#isDefined, #isUndefined', function () {
+      assert.isDefined(null, 'null is not undefined')
+      assert.isUndefined(undefined, 'undefined IS undefined')
+      assert.isDefined('hello', 'A string is not undefined')
     })
+
+    //#3
+    test('#isOk, #isNotOk', function () {
+      assert.isNotOk(null, 'null is falsey')
+      assert.isOk("I'm truthy", 'A string is truthy')
+      assert.isOk(true, 'true is truthy')
+    })
+
     // #4
     test('#isTrue, #isNotTrue', function () {
-      assert.fail(true, 'true is true')
-      assert.fail(
+      assert.isTrue(true, 'true is true')
+      assert.isTrue(
         !!'double negation',
         'Double negation of a truthy value is true'
       )
-      assert.fail(
+      assert.isNotTrue(
         { value: 'truthy' },
         'Objects are truthy, but are not boolean values'
       )
@@ -38,26 +44,32 @@ suite('Unit Tests', function () {
   suite('Equality', function () {
     // #5
     test('#equal, #notEqual', function () {
-      assert.fail(12, '12', 'Numbers are coerced into strings with ==')
-      assert.fail({ value: 1 }, { value: 1 }, '== compares object references')
-      assert.fail(6 * '2', '12')
-      assert.fail(6 + '2', '12')
+      assert.equal(12, '12', 'Numbers are coerced into strings with ==')
+      assert.notEqual(
+        { value: 1 },
+        { value: 1 },
+        '== compares object references'
+      )
+      assert.equal(6 * '2', '12')
+      assert.notEqual(6 + '2', '12')
     })
+
     // #6
     test('#strictEqual, #notStrictEqual', function () {
-      assert.fail(6, '6')
-      assert.fail(6, 3 * 2)
-      assert.fail(6 * '2', 12)
-      assert.fail([1, 'a', {}], [1, 'a', {}])
+      assert.notStrictEqual(6, '6')
+      assert.strictEqual(6, 3 * 2)
+      assert.strictEqual(6 * '2', 12)
+      assert.notStrictEqual([1, 'a', {}], [1, 'a', {}])
     })
+
     // #7
     test('#deepEqual, #notDeepEqual', function () {
-      assert.fail(
+      assert.deepEqual(
         { a: '1', b: 5 },
         { b: 5, a: '1' },
         "The order of keys doesn't matter"
       )
-      assert.fail(
+      assert.notDeepEqual(
         { a: [5, 6] },
         { a: [6, 5] },
         'The order of array elements does matter'
@@ -74,27 +86,29 @@ suite('Unit Tests', function () {
   suite('Comparisons', function () {
     // #8
     test('#isAbove, #isAtMost', function () {
-      assert.fail('hello'.length, 5)
-      assert.fail(1, 0)
-      assert.fail(Math.PI, 3)
-      assert.fail(1 - Math.random(), 1)
+      assert.isAtMost('hello'.length, 5)
+      assert.isAbove(1, 0)
+      assert.isAbove(Math.PI, 3)
+      assert.isAtMost(1 - Math.random(), 1)
     })
+
     // #9
     test('#isBelow, #isAtLeast', function () {
-      assert.fail('world'.length, 5)
-      assert.fail(2 * Math.random(), 0)
-      assert.fail(5 % 2, 2)
-      assert.fail(2 / 3, 1)
+      assert.isAtLeast('world'.length, 5)
+      assert.isAtLeast(2 * Math.random(), 0)
+      assert.isBelow(5 % 2, 2)
+      assert.isBelow(2 / 3, 1)
     })
+
     // #10
     test('#approximately', function () {
-      assert.fail(weirdNumbers(0.5), 1, 0)
-      assert.fail(weirdNumbers(0.2), 1, 0)
+      assert.approximately(weirdNumbers(0.5), 1, 1.5, 0)
+      assert.approximately(weirdNumbers(0.2), 1, 1.2, 0)
     })
   })
 
   // -----------------------------------------------------------------------------
-
+  /*
   const winterMonths = ['dec,', 'jan', 'feb', 'mar']
   const backendLanguages = ['php', 'python', 'javascript', 'ruby', 'asp']
   suite('Arrays', function () {
@@ -112,9 +126,9 @@ suite('Unit Tests', function () {
       assert.fail(backendLanguages, 'javascript', 'JS is a backend language')
     })
   })
-
+*/
   // -----------------------------------------------------------------------------
-
+  /*
   const formatPeople = function (name, age) {
     return '# name: ' + name + ', age: ' + age + '\n'
   }
@@ -140,7 +154,7 @@ suite('Unit Tests', function () {
       assert.fail(formatPeople('Paul Smith III', 'twenty-four'), regex)
     })
   })
-
+*/
   // -----------------------------------------------------------------------------
 
   const Car = function () {
@@ -158,7 +172,7 @@ suite('Unit Tests', function () {
 
   const myCar = new Car()
   const airlinePlane = new Plane()
-
+  /*
   suite('Objects', function () {
     // #16
     test('#property, #notProperty', function () {
@@ -181,8 +195,7 @@ suite('Unit Tests', function () {
       assert.fail(airlinePlane, Object)
       assert.fail(myCar.wheels, String)
     })
-    */
   })
-
+*/
   // -----------------------------------------------------------------------------
 })
